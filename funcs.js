5f9466800ab5cb2d1ed129595e4318447b8696c0
Robby Morgan <robby.d.morgan@gmail.com>
2023-09-25 16:38:28 -0500

    final submit

--- funcs.js
+++ funcs.js
@@ -1,13 +1,28 @@
 function isFive(num) {
-  // Your code here
+  if (num === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isOdd(number) {
-  // Your code here
+
+  if (typeof number !== 'number') {
+    throw new Error('Please enter a number');
+  } else if (number % 2 !== 0) {
+    return true;
+  }
+  return false;
+  
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let newRange = [];
+  for (let i = min; i <= max; i +=step) {
+    newRange.push(i);
+  }
+  return newRange;
 }
 
 
--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,37 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let word;
+  
+  this.beforeEach(() => {
+    word = new Word('word');
+  })
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+    
+      expect(word.word).to.exist;
     });
   
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.equal('word');
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeVowels()).to.equal('wrd');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeConsonants()).to.equal('o');
     });
   });
   
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(word.pigLatin()).to.equal('ordway');
     });
   });
 });
